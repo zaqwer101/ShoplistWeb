1f50ea1aefbe5a5dc70d3f4571cb5bdec4f66463
zaqwer101 <platonov256@gmail.com>
2020-06-26 21:06:13 +0800

    Родные чекбоксары

--- www-root/script.js
+++ www-root/script.js
@@ -4,36 +4,57 @@ var example2 = new Vue
     data: {
       user: '',
       password: '',
-      token: ''
+      token: '',
+      items: [],
+      bought: []
     },
 
     methods: {
-      getToken: function (event) 
-      {
-        if (this.user != '' && this.password != '')
-        {
-          axios.post('https://127.0.0.1/auth',
-          {
+      getToken: function (event) {
+        if (this.user != '' && this.password != '') {
+          axios.post('https://127.0.0.1/auth', {
             // POST data
             user: this.user, 
             password: this.password
           })
-          .then( (response) => 
-          {
+
+          .then( (response) => {
             // If everything is OK
             this.token = response.data.token
             console.log("Token: " + this.token)
-          }, (error) => 
-          {
+          }, 
+          
+          (error) => {
             // If an error occurred
+            this.token = ''
             console.log(error); 
-            alert(error);
+            alert("Can not get token");
           })
+        } else {
+          alert('Incorrect data');
         }
-        else
-        {
-          alert('Incorrect data')
+      },
+      getItems: function (event) {
+        if (this.token == '') {
+          // If token was not set
+          this.getToken(null)
+          if (this.token == '') return
         }
+        axios.get('https://127.1/shoplist?token=' + this.token)
+        .then( (response) => {
+          // if ok
+          this.items = response.data
+          this.items.forEach(item => {
+            console.log("Item - " + item.name + ":" + item.bought)
+            if (item.bought == "true") {
+              this.bought.push(item.name)
+            }
+          });
+        },
+        (error) => {
+          alert("Can not get items");
+          console.log(error)
+        })
       }
     }
   })
