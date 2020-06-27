f4af2ff26d14a1b2449ce4bbfb8551a1f982a240
zaqwer101 <platonov256@gmail.com>
2020-06-27 15:07:47 +0800

    Теперь чекбоксы блокируются

--- www-root/script.js
+++ www-root/script.js
@@ -1,18 +1,18 @@
-var example2 = new Vue
-({
-    el: '#example',
-    data: {
-      user: '',
-      password: '',
-      token: '',
-      items: [],
-      bought: []
-    },
+var example2 = new Vue({
+  el: '#example',
+  data: {
+    address: '127.0.0.1',
+    user: '',
+    password: '',
+    token: '',
+    items: [],
+    bought: []
+  },
 
-    methods: {
-      getToken: function (event) {
-        if (this.user != '' && this.password != '') {
-          axios.post('https://127.0.0.1/auth', {
+  methods: {
+    getToken: function(event) {
+      if (this.user != '' && this.password != '') {
+          axios.post('https://' + this.address + '/auth', {
             // POST data
             user: this.user, 
             password: this.password
@@ -21,40 +21,41 @@ var example2 = new Vue
           .then( (response) => {
             // If everything is OK
             this.token = response.data.token
-            console.log("Token: " + this.token)
+            console.log('Token: ' + this.token)
           }, 
           
           (error) => {
-            // If an error occurred
-            this.token = ''
-            console.log(error); 
-            alert("Can not get token");
+      // If an error occurred
+      this.token = ''
+      console.log(error);
+      alert('Can not get token');
           })
-        } else {
-          alert('Incorrect data');
-        }
-      },
-      getItems: function (event) {
-        if (this.token == '') {
-          // If token was not set
-          this.getToken(null)
-          if (this.token == '') return
-        }
-        axios.get('https://127.1/shoplist?token=' + this.token)
-        .then( (response) => {
-          // if ok
-          this.items = response.data
-          this.items.forEach(item => {
-            console.log("Item - " + item.name + ":" + item.bought)
-            if (item.bought == "true") {
-              this.bought.push(item.name)
-            }
-          });
-        },
-        (error) => {
-          alert("Can not get items");
-          console.log(error)
-        })
+      } else {
+        alert('Incorrect data');
       }
+    },
+    getItems: function(event) {
+      if (this.token == '') {
+        // If token was not set
+        this.getToken(null)
+        if (this.token == '') return
+      }
+      axios.get('https://' + this.address + '/shoplist?token=' + this.token)
+          .then(
+              (response) => {
+                // if ok
+                this.items = response.data;
+                this.items.forEach(item => {
+                  console.log('Item - ' + item.name + ':' + item.bought)
+                  if (item.bought == 'true') {
+                    this.bought.push(item.name)
+                  }
+                });
+              },
+              (error) => {
+                alert('Can not get items');
+                console.log(error)
+              })
     }
-  })
+  }
+})
