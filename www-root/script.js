9dbed0bab7d09cba66d4cdb438d6fb7525b94d32
zaqwer101 <platonov256@gmail.com>
2020-07-10 15:00:12 +0800

    аутентификация и получение списка

--- www-root/script.js
+++ www-root/script.js
@@ -10,9 +10,62 @@ var example2 = new Vue({
   },
 
   methods: {
-    auth: function() {
 
+    // сделать запрос к API
+    request: function(endpoint, method, data) {
+      if (method == 'post') {
+        return axios.post(this.address + endpoint, data)
+          .then(r => r)
+          .catch(err => console.error(err))
+      }
+
+      if (method == 'get') {
+        return axios.get(this.address + endpoint + '?' + data)
+          .then(r => r)
+          .catch(err => console.error(err))
+      }
+    },
+
+    // получить токен
+    getToken: function(func = null /* функция, которую выполнить после получения токена */) {
+      this.request('/auth', 'post', { user: this.user, password: this.password })
+        .then(r => {
+          this.token = r.data.token;
+          console.log("Токен: " + r.data.token);
+          if (func != null) func();
+        })
+        .catch(err => { 
+          alert("Аутентификация не удалась!")
+        })
     }, 
+
+    // провести аутентификацию и показать основную форму
+    auth: function() {
+      if (this.user != '' && this.password != '') {
+        this.getItems(); // getItems уже позовёт getToken, если токена нет
+      }
+    },
+
+    // получить и заполнить список предметов
+    getItems: function() {
+      if (this.token == '') {
+        console.log("Токен не задан");
+        this.getToken(this.getItems);
+        if (this.token == '') {
+          return;
+        }
+      } else {
+        this.request('/shoplist', 'get', 'token=' + this.token)
+          .then(r => {
+            if (r.data.length != 0) console.log("Список предметов:")
+            r.data.forEach(item => {
+              console.log(item.name + ": " + item.bought);
+              this.items.push(item);
+              if (item.bought == 'true') this.bought.push(item.name);
+            });
+          })
+      }
+    },
 
     register: function() {
 
@@ -22,8 +75,8 @@ var example2 = new Vue({
 
     },
 
-    onDelete: function(event) {
-      
+    deleteItem: function(event) {
+
     }
   }
 })
