fc17f72d3409bf933ba83c14bf4252c8ded89698
zaqwer101 <platonov256@gmail.com>
2021-02-27 17:58:37 +0800

    Добавил TTL токена Поправил аутентификацию

--- www-root/script.js
+++ www-root/script.js
@@ -1,17 +1,20 @@
 var example2 = new Vue({
   el: '#shoplist',
   data: {
-    address: 'https://palearis.cloud',
+    address: 'https://127.0.0.1',
     user: '',
     password: '',
     itemName: '',
     token: '',
     items: [],
     bought: [],
+    token_ttl: 0,  // время жизни токена
+    token_date: 0  // unixtime время получения токена
   },
 
   created: function () {
     console.log("Инстанс ожил");
+    this.getServerInfo();
     if (this.loadToken()) {
       this.getItems();
     }
@@ -19,8 +22,8 @@ var example2 = new Vue({
 
   methods: {
 
-    // сделать запрос к API
-    request: function(endpoint, method, data) {
+    // сделать базовый запрос к API
+    request: function(endpoint, method, data = null) {
       if (method == 'post') {
         return axios.post(this.address + endpoint, data)
         .then(r => r)
@@ -28,9 +31,16 @@ var example2 = new Vue({
       }
 
       if (method == 'get') {
-        return axios.get(this.address + endpoint + '?' + data)
-        .then(r => r)
-        .catch(err => console.error(err));
+        if (data != null) {
+          return axios.get(this.address + endpoint + '?' + data)
+            .then(r => r)
+            .catch(err => console.error(err));
+        }
+        else {
+          return axios.get(this.address + endpoint)
+            .then(r => r)
+            .catch(err => console.error(err));
+        }
       }
 
       if (method == 'delete') {
@@ -40,6 +50,32 @@ var example2 = new Vue({
       }
     },
 
+    // сделать запрос с авторизацией
+    authorizedRequest: function(endpoint, method, data = null)
+    {
+      var timeDiff = parseInt(Date.now() / 1000) - this.token_date;
+      console.log("TTL токена - " + this.token_ttl)
+      console.log("С момента получения токена прошло " + timeDiff);
+
+      if (!this.isTokenValid()) { // значит токен не токен, получаем новый
+        console.log("Токену осталось жить " + (this.token_ttl - timeDiff))
+        this.getToken();
+      }
+      
+      var rdata;
+      if (method == 'post' || method == 'delete') { // если запрос POST/DELETE - добавляем токен в данные
+        rdata = data;
+        rdata.token = this.token; 
+      } else { // если запрос не POST/DELETE - добавляем токен в аргументы
+        if (rdata != null) rdata = data + "&token=" + this.token;
+        else rdata = "token=" + this.token;
+      }
+      console.log("Изначальные данные запроса - " + data);
+      console.log("Итоговые данные запроса - " + rdata); 
+
+      return this.request(endpoint, method, rdata)
+    },
+
     // получить токен
     getToken: function(func = null /* функция, которую выполнить после получения токена */) {
       this.request('/auth', 'post', { user: this.user, password: this.password })
@@ -47,7 +83,9 @@ var example2 = new Vue({
         this.token = r.data.token;
         console.log("Токен: " + r.data.token);
         this.saveToken();
-        if (func != null) func();
+        if (func != null){
+          func();
+        }
       })
       .catch(err => { 
         alert("Аутентификация не удалась!")
@@ -57,7 +95,7 @@ var example2 = new Vue({
     // провести аутентификацию и показать основную форму
     auth: function() {
       if (this.user != '' && this.password != '') {
-        this.getItems(); // getItems уже позовёт getToken, если токена нет
+        this.getToken(this.getItems); 
       }
     },
 
@@ -80,23 +118,15 @@ var example2 = new Vue({
 
     // получить и заполнить список элементов
     getItems: function() {
-      if (this.token == '') {
-        console.log("Токен не задан");
-        this.getToken(this.getItems);
-        if (this.token == '') {
-          return;
-        }
-      } else {
-        this.request('/shoplist', 'get', 'token=' + this.token)
-        .then(r => {
-          if (r.data.length != 0) console.log("Список элементов:")
-          this.items = r.data;
-          r.data.forEach(item => {
-            console.log(item.name + ": " + item.bought);
-            if (item.bought == 'true') this.bought.push(item.name);
-          });
-        })
-      }
+      this.authorizedRequest('/shoplist', 'get')
+      .then(r => {
+        if (r.data.length != 0) console.log("Список элементов:")
+        this.items = r.data;
+        r.data.forEach(item => {
+          console.log(item.name + ": " + item.bought);
+          if (item.bought == 'true') this.bought.push(item.name);
+        });
+      })
     },
 
     // функция, вызываемая при чеке элемента
@@ -110,8 +140,7 @@ var example2 = new Vue({
         checked = 'false';
       }
 
-      this.request('/shoplist/bought', 'post', {
-        token: this.token,
+      this.authorizedRequest('/shoplist/bought', 'post', {
         name: event.target.id,
         bought: checked
       })
@@ -121,8 +150,7 @@ var example2 = new Vue({
     deleteItem: function(event) {
       itemName = event.target.id.split('_')[1];
       console.log(itemName + ' clicked delete');
-      this.request('/shoplist', 'delete', {
-        token: this.token,
+      this.authorizedRequest('/shoplist', 'delete', {
         name: itemName}).then(
         () => {
           this.getItems();
@@ -134,7 +162,7 @@ var example2 = new Vue({
     addItem: function() {
       if (this.itemName == '') alert('Имя не может быть пустым')
       else {
-        this.request('/shoplist', 'post', { token: this.token, name: this.itemName })
+        this.authorizedRequest('/shoplist', 'post', { name: this.itemName })
         .then(() => {
           this.getItems();
         });
@@ -142,18 +170,39 @@ var example2 = new Vue({
     }, 
 
     saveToken: function() {
+      this.token_date = parseInt(Date.now() / 1000);
       localStorage.token = this.token; 
-      console.log("Токен сохранен");
+      localStorage.token_date = this.token_date;
+      console.log("Токен сохранен: " + this.token_date);
     }, 
 
     loadToken: function() {
       if (localStorage.token) {
         this.token = localStorage.token;
+        this.token_date = Number(localStorage.token_date); 
+        this.token_ttl = Number(localStorage.token_ttl);
         console.log("Токен загружен");
         return true;
       } 
       console.log("Не удалось загрузить токен");
       return false;
+    },
+
+    getServerInfo: function() {
+      console.log("Получаем информацию о сервере");
+      if (this.token_ttl == 0) // значит надо получить ttl токена
+      {
+        this.request('/info', 'get').then( r => {
+          this.token_ttl = Number(r.data.auth.token_ttl); 
+          localStorage.token_ttl = this.token_ttl;
+          console.log("Получен TTL токена - " + this.token_ttl);
+        })
+      }
+    },
+
+    isTokenValid: function() {
+      var timeDiff = parseInt(Date.now() / 1000) - this.token_date;
+      return timeDiff < this.token_ttl && this.token != '';
     }
   }
 })
